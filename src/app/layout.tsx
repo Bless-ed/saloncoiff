cd2e46747eb6b4176b07e6428ef89dd7b1159ef6
Eudes-Dev <euudesdev@gmail.com>
2025-03-30 17:51:41 +0000

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           <div className="flex-1 flex flex-col h-full">
             {children}
           </div>
+          <Footer />
         </main>
       </body>
     </html>
