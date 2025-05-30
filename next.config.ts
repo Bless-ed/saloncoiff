1df6264ddded80fd4c855c0c97b51161153ff516
Bless_ed <belieberblessed@gmail.com>
2025-05-30 08:02:46 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   eslint: {
-    ignoreDuringBuilds: true, // ⛔ Ignorer les erreurs ESLint au build (temporairement)
-  },
   /* config options here */
 };
 
